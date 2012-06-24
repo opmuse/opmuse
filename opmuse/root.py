9887893114bffdc0f97507c3a104fdb8958729f7
Mattias Fliesberg <mattias.fliesberg@gmail.com>
2012-06-24 19:17:49 +0200

    implemented less css using lesscpy

--- opmuse/root.py
+++ opmuse/root.py
@@ -1,6 +1,29 @@
 import os, cherrypy
+from lesscpy.lessc import parser
+
+class Styles(object):
+    @cherrypy.expose
+    def default(self, file):
+        cherrypy.response.headers['Content-Type'] = 'text/css'
+
+        p = parser.LessParser()
+        p.parse(
+            filename=os.path.join(os.path.abspath("."), "public", "styles", file),
+            debuglevel=0
+        )
+
+        items = {
+            'nl': '\n',
+            'tab': '\t',
+            'ws': ' ',
+            'eb': '\n'
+        }
+
+        return ''.join([u.fmt(items) for u in p.result if u]).strip()
 
 class Root(object):
+    styles = Styles()
+
     @cherrypy.expose
     @cherrypy.tools.jinja(filename='index.html')
     def index(self):
