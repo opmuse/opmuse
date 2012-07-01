c3b7fbb4c1ac6e08b8074dc2942d475d1677d1ab
Mattias Fliesberg <mattias.fliesberg@gmail.com>
2012-07-01 20:06:08 +0200

    use one player for all tracks

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,2 +1,10 @@
-require(['jquery'], function() {
+require(['jquery'], function($) {
+    $('#library .track .play').click(function (event) {
+        var url = $(this).attr('href');
+        var player = $('#player').get(0);
+        player.src = url;
+        player.load();
+        player.play();
+        return false;
+    });
 });
