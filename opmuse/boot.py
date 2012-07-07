9b632a174185454e1ef724a02d385c821e91b4b0
Viktor Thand <viktor.thand@gmail.com>
2012-07-07 14:34:53 +0200

    Implement server name setting and show it in headings and page title
    Also put the current page name in the page title and make touched files conform
    to PEP8

--- opmuse/boot.py
+++ opmuse/boot.py
@@ -1,6 +1,6 @@
 import cherrypy
 from os.path import join, abspath, dirname
-from opmuse.jinja import Jinja
+from opmuse.jinja import Jinja, env
 from opmuse.library import LibraryPlugin
 
 if __name__ == '__main__':
@@ -8,18 +8,21 @@ if __name__ == '__main__':
     import opmuse.root
 
     cherrypy.tree.mount(opmuse.root.Root(), '/', {
-        '/': { },
+        '/': {},
         '/scripts': {
             'tools.staticdir.on': True,
-            'tools.staticdir.dir': join(abspath(dirname(__file__)), "../public/scripts"),
+            'tools.staticdir.dir': join(abspath(dirname(__file__)),
+                                        "../public/scripts"),
         }
     })
 
     cherrypy.engine.library = LibraryPlugin(cherrypy.engine)
     cherrypy.engine.library.subscribe()
 
-    config = cherrypy._cpconfig.Config(file=join(abspath(dirname(__file__)), "../config/opmuse.ini"))
+    config = cherrypy._cpconfig.Config(file=join(abspath(dirname(__file__)),
+                                                 "../config/opmuse.ini"))
     cherrypy.config.update(config)
+    env.globals['server_name'] = cherrypy.config['opmuse']['server_name']
 
     cherrypy.engine.start()
     cherrypy.engine.block()
