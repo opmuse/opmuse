ca6026e598bf000e88b3ea03e6065c1057bd756f
Mattias Fliesberg <mattias.fliesberg@gmail.com>
2012-07-23 14:15:31 +0200

    update library unit test

--- tests/library.py
+++ tests/library.py
@@ -1,12 +1,25 @@
 import os, unittest
-from opmuse.library import Library
+from opmuse.library import Library, Artist
+from opmuse.database import Base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 class LibraryTest(unittest.TestCase):
     def test_tracks(self):
-        library = Library(os.path.join(os.path.dirname(__file__), "../sample_library"))
-        tracks = library.get_tracks()
+        db_path = 'tests/opmuse.db'
 
-        self.assertEqual(tracks[0].artist.name, "opmuse")
-        self.assertEqual(tracks[0].album.name, "opmuse")
-        self.assertEqual(tracks[0].name, "opmuse")
+        if os.path.exists(db_path):
+            os.remove(db_path)
+
+        engine = create_engine('sqlite:///./%s' % db_path)
+        Base.metadata.create_all(engine)
+        session = sessionmaker(bind=engine)()
+
+        Library(os.path.join(os.path.dirname(__file__), "../sample_library"), session)
+
+        artist = session.query(Artist).one()
+
+        self.assertEqual(artist.name, "opmuse")
+        self.assertEqual(artist.albums[0].name, "opmuse")
+        self.assertEqual(artist.albums[0].tracks[0].name, "opmuse")
 
