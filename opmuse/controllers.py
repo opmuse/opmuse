7050919f1a019686dc68f1386b5cccf331144060
Mattias Fliesberg <mattias.fliesberg@gmail.com>
2012-10-20 13:34:15 +0200

    raise a 409 when there's no tracks in the queue instead of 404

--- opmuse/controllers.py
+++ opmuse/controllers.py
@@ -541,7 +541,7 @@ class Root(object):
         track = queue_dao.get_next_track(user_id, repeat = False)
 
         if track is None:
-            raise cherrypy.NotFound()
+            raise cherrypy.HTTPError(status=409)
 
         user_agent = cherrypy.request.headers['User-Agent']
 
