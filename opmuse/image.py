5f4aedf91c6c8a3407eb5180888631a0cb9cc1aa
Mattias Fliesberg <mattias.fliesberg@gmail.com>
2012-11-11 21:07:10 +0100

    crop image properly if height > width

--- opmuse/image.py
+++ opmuse/image.py
@@ -9,12 +9,32 @@ class Image:
             height = width
 
         process = subprocess.Popen([
-                'convert',
-                '-resize', '>x%d' % height,
-                '-gravity', 'center',
-                '-extent', '%dx%d' % (width, height),
-                source, dest
-            ], shell = False, stdout = subprocess.PIPE, stderr = self.FNULL, stdin = None)
+            'identify',
+            '-format',
+            '%w %h',
+            source
+        ], shell = False, stdout = subprocess.PIPE, stderr = self.FNULL, stdin = None)
+
+        process.wait()
+
+        dimension = process.stdout.read()
+
+        source_width, source_height = dimension.strip().split(b' ')
+
+        source_width, source_height = int(source_width), int(source_height)
+
+        if source_width > source_height:
+            resize = '>x%d' % height
+        else:
+            resize = '%dx>' % width
+
+        process = subprocess.Popen([
+            'convert',
+            '-resize', resize,
+            '-gravity', 'center',
+            '-extent', '%dx%d' % (width, height),
+            source, dest
+        ], shell = False, stdout = self.FNULL, stderr = self.FNULL, stdin = None)
 
         process.wait()
 
