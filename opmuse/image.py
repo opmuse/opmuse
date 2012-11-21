ca4b65fec742cf44315d9404610a646c819c67e2
Mattias Fliesberg <mattias.fliesberg@gmail.com>
2012-11-21 20:27:29 +0100

    resize proper without lots of crud :|

--- opmuse/image.py
+++ opmuse/image.py
@@ -9,34 +9,8 @@ class Image:
             height = width
 
         process = subprocess.Popen([
-            'identify',
-            '-format',
-            '%w %h,',
-            source
-        ], shell = False, stdout = subprocess.PIPE, stderr = self.FNULL, stdin = None)
-
-        process.wait()
-
-        dimension = process.stdout.read().strip()
-
-        resize = None
-
-        if len(dimension) > 0:
-            # in case of i.e. gifs there might be multiple frames so split by ","
-            # first and then just use the first frame for our source width/height
-            source_width, source_height = dimension.split(b',')[0].split(b' ')
-
-            source_width, source_height = int(source_width), int(source_height)
-
-            if source_width < source_height:
-                resize = '%dx<' % width
-
-        if resize is None:
-            resize = '>x%d' % height
-
-        process = subprocess.Popen([
             'convert',
-            '-resize', resize,
+            '-resize', '%dx%d^' % (width, height),
             '-gravity', 'center',
             '-extent', '%dx%d' % (width, height),
             source, dest
