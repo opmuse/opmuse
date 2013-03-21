e4b4178fd6b269d09947693c767180df3366e13e
Mattias Fliesberg <mattias.fliesberg@gmail.com>
2013-03-21 21:38:44 +0100

    try multiple wikipedia langs, right now en and sv

--- opmuse/wikipedia.py
+++ opmuse/wikipedia.py
@@ -4,12 +4,13 @@ from urllib import request, parse
 
 
 class Wikipedia():
-    BASE_URL = 'http://en.wikipedia.org'
+    BASE_URL = 'http://%s.wikipedia.org'
+    LANGUAGES = ['en', 'sv']
     BASE_API_URL = '%s/w/api.php' % BASE_URL
     BASE_TITLE_URL = '%s/wiki' % BASE_URL
 
     def get_track(self, artist_name, album_name, track_name):
-        extract = self.find_extract(track_name, ['song'])
+        extract = self.find_extract(track_name, ['song'], Wikipedia.LANGUAGES)
 
         if extract is None:
             summary = ''
@@ -33,10 +34,8 @@ class Wikipedia():
             album_name = artist_name
 
         if album_name is not None and artist_name is not None:
-            extract = self.find_extract(album_name, ['%s album' % artist_name, 'album', 'ep', 'soundtrack'])
-
-            if extract is None:
-                extract = self.find_extract('%s - %s' % (artist_name, album_name))
+            extract = self.find_extract(album_name, ['%s album' % artist_name, 'album', 'ep', 'soundtrack'],
+                                        Wikipedia.LANGUAGES)
 
         if extract is None:
             summary = ''
@@ -55,7 +54,7 @@ class Wikipedia():
 
     def get_artist(self, artist_name):
 
-        extract = self.find_extract(artist_name, ['band', 'musician', 'singer', 'artist'])
+        extract = self.find_extract(artist_name, ['band', 'musician', 'singer', 'artist'], Wikipedia.LANGUAGES)
 
         if extract is None:
             summary = ''
@@ -72,51 +71,54 @@ class Wikipedia():
             'url': url
         }
 
-    def find_extract(self, name, types = []):
-        extract = None
-
+    def find_extract(self, name, types, languages):
         title_format = "%s (%s)"
 
-        for type in types:
-            extract = self.query_extracts(title_format % (name, type))
-
-            if extract is not None:
-                break
-
-        if extract is None:
-            title = None
+        for language in languages:
+            extract = None
 
             for type in types:
-                title = self.opensearch(title_format % (name, type))
+                extract = self.query_extracts(title_format % (name, type), language)
 
-                if title is not None:
+                if extract is not None:
                     break
 
-            if title is None:
-                title = self.opensearch(name)
+            if extract is None:
+                title = None
 
-            if title is not None:
-                extract = self.query_extracts(title)
+                for type in types:
+                    title = self.opensearch(title_format % (name, type), language)
 
-        if extract is None:
-            extract = self.query_extracts(name)
+                    if title is not None:
+                        break
 
-        return extract
+                if title is None:
+                    title = self.opensearch(name, language)
 
-    def opensearch(self, query):
+                if title is not None:
+                    extract = self.query_extracts(title, language)
+
+            if extract is None:
+                extract = self.query_extracts(name, language)
+
+            if extract is not None:
+                return extract
+
+
+    def opensearch(self, query, language):
         response = self._request({
             "action": "opensearch",
             "search": query,
             "format": "json",
             "limit": 1
-        })
+        }, language)
 
         if len(response) > 1 and len(response[1]) > 0:
             return response[1][0]
         else:
             return None
 
-    def query_extracts(self, title):
+    def query_extracts(self, title, language):
         response = self._request({
             "action": "query",
             "prop": "extracts",
@@ -124,7 +126,7 @@ class Wikipedia():
             "exsentences": 20,
             "titles": title,
             "redirects": ""
-        })
+        }, language)
 
         response = response['query']['pages'].popitem()[1]
 
@@ -132,16 +134,16 @@ class Wikipedia():
             return {
                 'extract': response['extract'],
                 'title': response['title'],
-                'url': self._to_url(response['title'])
+                'url': self._to_url(response['title'], language)
             }
         else:
             return None
 
-    def _to_url(self, title):
-        return "%s/%s" % (Wikipedia.BASE_TITLE_URL, parse.quote(title))
+    def _to_url(self, title, language):
+        return "%s/%s" % (Wikipedia.BASE_TITLE_URL % language, parse.quote(title))
 
-    def _request(self, params):
-        response = request.urlopen("%s?%s" % (Wikipedia.BASE_API_URL, parse.urlencode(params)))
+    def _request(self, params, language):
+        response = request.urlopen("%s?%s" % (Wikipedia.BASE_API_URL % language, parse.urlencode(params)))
         return json.loads(response.read().decode("utf8"))
 
 
