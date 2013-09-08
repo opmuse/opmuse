683dab7fec623b9a4ec802770b7427ac6b836c91
Mattias Fliesberg <mattias.fliesberg@gmail.com>
2013-09-08 20:32:18 +0200

    use user-agent header for "is playing" check
    so skip works properly...

--- opmuse/controllers.py
+++ opmuse/controllers.py
@@ -1364,11 +1364,11 @@ class Stream:
         cherrypy.engine.subscribe('transcoding.end', self.transcoding_end)
         cherrypy.engine.subscribe('transcoding.start', self.transcoding_start)
 
-    def transcoding_start(self, track):
-        Stream.STREAM_PLAYING[cherrypy.request.user.id] = True
+    def transcoding_start(self, transcoder, track):
+        Stream.STREAM_PLAYING[cherrypy.request.user.id] = cherrypy.request.headers['User-Agent']
 
     def transcoding_end(self, track, transcoder):
-        Stream.STREAM_PLAYING[cherrypy.request.user.id] = False
+        Stream.STREAM_PLAYING[cherrypy.request.user.id] = None
 
     @cherrypy.expose
     @cherrypy.tools.transcodingsubprocess()
@@ -1383,16 +1383,17 @@ class Stream:
         if 'dead' in kwargs and kwargs['dead'] == 'true':
             raise cherrypy.HTTPError(status=503)
 
+        user_agent = cherrypy.request.headers['User-Agent']
+
         # allow only one streaming client at once, or weird things might occur
-        if user.id in Stream.STREAM_PLAYING and Stream.STREAM_PLAYING[user.id] is True:
+        if (user.id in Stream.STREAM_PLAYING and Stream.STREAM_PLAYING[user.id] is not None and
+            Stream.STREAM_PLAYING[user.id] != user_agent):
             raise cherrypy.HTTPError(status=503)
 
         queue = queue_dao.get_next(user.id)
 
         if queue is None:
             raise cherrypy.HTTPError(status=409)
-
-        user_agent = cherrypy.request.headers['User-Agent']
 
         transcoder, format = transcoding.determine_transcoder(
             queue.track,
