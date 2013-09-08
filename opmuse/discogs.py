640867333f2f48d7ac646cb800ae0186f0533b9f
Mattias Fliesberg <mattias.fliesberg@gmail.com>
2013-09-08 10:22:25 +0200

    catch and log exceptions in discogs

--- opmuse/discogs.py
+++ opmuse/discogs.py
@@ -1,7 +1,13 @@
 import discogs_client
 from discogs_client import HTTPError
+from requests.exceptions import ConnectionError
+
 
 discogs_client.user_agent = 'opmuse/DEV'
+
+
+def log(msg):
+    cherrypy.log(msg, context='discogs')
 
 
 class Discogs:
@@ -16,9 +22,10 @@ class Discogs:
             for alias in discogs_artist.aliases:
                 artist['aliases'].append(alias.name)
 
-        except HTTPError:
-            pass
+        except (HTTPError, ConnectionError) as error:
+            log("Failed to fetch artist: %s" % error)
 
         return artist
+
 
 discogs = Discogs()
