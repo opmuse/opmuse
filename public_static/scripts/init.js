504ec60c2db5ffb85967f6da22314e2ec3f5d3d6
Mattias Fliesberg <mattias.fliesberg@gmail.com>
2013-09-27 18:40:02 +0200

    make sure cache-bustin' works for data-main script

---
+++ public_static/scripts/init.js
@@ -0,0 +1,27 @@
+window.require = {
+    baseUrl: "/static/scripts/",
+    urlArgs: "version=" + window.opmuseGlobals.version,
+    paths: {
+        'jquery.nanoscroller': 'lib/jquery.nanoscroller',
+        'jquery.fileupload': 'lib/jquery.fileupload',
+        'jquery.ui.widget': 'lib/jquery.ui.widget',
+        'jquery': 'lib/jquery',
+        'bootstrap': 'lib/bootstrap',
+        'domReady': 'lib/domReady',
+        'sprintf': 'lib/sprintf',
+        'moment': 'lib/moment',
+        'blur': 'lib/blur',
+        'matchMedia': 'lib/matchMedia',
+        'jquery.ui': 'lib/jquery.ui',
+        'typeahead': 'lib/typeahead'
+    },
+    shim: {
+        'typeahead': ['jquery'],
+        'jquery.ui': ['jquery'],
+        'blur': ['jquery'],
+        'bootstrap/popover': ['bootstrap/tooltip'],
+        'bootstrap/button': ['bootstrap/dropdown'],
+        'jquery.nanoscroller': ['jquery']
+    },
+    waitSeconds: 30
+};
--- public_static/scripts/main.js
+++ public_static/scripts/main.js
@@ -1,31 +1,3 @@
-require.config({
-    baseUrl: "/static/scripts/",
-    urlArgs: "version=" + window.opmuseGlobals.version,
-    paths: {
-        'jquery.nanoscroller': 'lib/jquery.nanoscroller',
-        'jquery.fileupload': 'lib/jquery.fileupload',
-        'jquery.ui.widget': 'lib/jquery.ui.widget',
-        'jquery': 'lib/jquery',
-        'bootstrap': 'lib/bootstrap',
-        'domReady': 'lib/domReady',
-        'sprintf': 'lib/sprintf',
-        'moment': 'lib/moment',
-        'blur': 'lib/blur',
-        'matchMedia': 'lib/matchMedia',
-        'jquery.ui': 'lib/jquery.ui',
-        'typeahead': 'lib/typeahead'
-    },
-    shim: {
-        'typeahead': ['jquery'],
-        'jquery.ui': ['jquery'],
-        'blur': ['jquery'],
-        'bootstrap/popover': ['bootstrap/tooltip'],
-        'bootstrap/button': ['bootstrap/dropdown'],
-        'jquery.nanoscroller': ['jquery']
-    },
-    waitSeconds: 30
-});
-
 requirejs.onError = function (err) {
     var overlay = document.getElementById("overlay");
     overlay.className = 'error';
