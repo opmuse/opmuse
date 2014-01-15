2cd76cb13bcbb1e6c861206655bc11e1b56f611c
Mattias Fliesberg <mattias.fliesberg@gmail.com>
2014-01-15 22:46:15 +0100

    move users/you to top level menu, and fancy it up a bit

--- opmuse/library.py
+++ opmuse/library.py
@@ -1302,7 +1302,7 @@ class LibraryProcess:
         # Disallow certain slug values that will conflict with urls.
         # Would be nice if this was automatic (search through controllers for all top-level url components).
         if string in ('library', 'users', 'upload', 'logout', 'login', 'search',
-                      'queue', 'cover', 'font', 'va', 'unknown', 'download'):
+                      'queue', 'cover', 'font', 'va', 'unknown', 'download', 'you'):
             index += 1
             return LibraryProcess.slugify(string, index)
 
