177a7415336bb695ccf5526fe6e270d1eeffea17
Mattias Fliesberg <mattias.fliesberg@gmail.com>
2014-02-15 17:51:43 +0100

    generate dbconfig config file

--- scripts/debian-after-install.sh
+++ scripts/debian-after-install.sh
@@ -2,12 +2,21 @@
 
 . /etc/default/opmuse
 
-getent passwd $USER > /dev/null
-
-if [ $? -ne 0 ]; then
+if ! getent passwd $USER > /dev/null ; then
     adduser --quiet --system --no-create-home --home $CACHEDIR --shell /usr/sbin/nologin $USER
 fi
 
 chown $USER $LOGDIR $CACHEDIR
+
+. /usr/share/debconf/confmodule
+. /usr/share/dbconfig-common/dpkg/postinst.mysql
+
+dbc_generate_include_owner="opmuse"
+dbc_generate_include_perms="0640"
+dbc_generate_include=template:/etc/opmuse/dbconfig.conf
+
+if ! dbc_go opmuse $@ ; then
+    echo 'Automatic configuration using dbconfig-common failed!'
+fi
 
 /etc/init.d/opmuse start
