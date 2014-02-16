4c2a98501b10d710f426b0279c0a732ce096baa3
Mattias Fliesberg <mattias.fliesberg@gmail.com>
2014-02-16 15:11:22 +0100

    add debconf inputs for library.path etc

--- scripts/build-python-deb.sh
+++ scripts/build-python-deb.sh
@@ -1,9 +1,9 @@
 #!/bin/zsh
 
-if [[ $# -ne 12 && $# -ne 13 ]]; then
+if [[ $# -ne 13 && $# -ne 14 ]]; then
     echo -n "Usage: $(basename $0) repo dist package_file package_name package_version "
     echo -n "package_before package_after package_deps package_confs package_init "
-    echo "package_default package_debconf [--no-prefix]"
+    echo "package_default package_debconf package_templates [--no-prefix]"
     exit 1
 fi
 
@@ -23,8 +23,9 @@ package_confs=$9
 package_init=$10
 package_default=$11
 package_debconf=$12
+package_templates=$13
 
-if [[ $13 == "--no-prefix" ]]; then
+if [[ $14 == "--no-prefix" ]]; then
     prefix=0
 else
     prefix=1
@@ -94,6 +95,12 @@ fi
 if [[ $package_debconf != "none" ]]; then
     args+=(
         --deb-config $package_debconf
+    )
+fi
+
+if [[ $package_templates != "none" ]]; then
+    args+=(
+        --deb-templates $package_templates
     )
 fi
 
