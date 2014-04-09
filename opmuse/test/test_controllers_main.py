5295562950fd7c035777c6b037f094a6034ac242
Mattias Fliesberg <mattias.fliesberg@gmail.com>
2014-04-09 23:40:51 +0200

    move play controller tests to main controller tests
    until we fix support for multiple controller test modules/classes

--- opmuse/test/test_controllers_main.py
+++ opmuse/test/test_controllers_main.py
@@ -1,8 +1,17 @@
 from . import WebCase
+from http.cookies import BaseCookie
 
 
 class MainTest(WebCase):
     setup_server = WebCase._opmuse_setup_server
+
+    def _login(self):
+        self.getPage('/login', method='POST', body='login=admin&password=admin')
+
+        cookie = BaseCookie()
+        cookie.load(self.cookies[0][1])
+
+        return cookie['auth_tkt'].value.strip("\"")
 
     def test_login_redirect(self):
         self.getPage("/")
@@ -11,7 +20,7 @@ class MainTest(WebCase):
         self.assertNoHeader('Location')
 
     def test_login(self):
-        self.getPage('/login', method='POST', body='login=admin&password=admin')
+        self._login()
         self.assertHeader('Location', 'http://127.0.0.1:54583/')
         self.assertHeaderMatches('Set-Cookie', 'auth_tkt="[0-9a-f]{40}admin!"')
         self.getPage('/', headers=self.cookies)
@@ -21,6 +30,23 @@ class MainTest(WebCase):
         self.assertMatchesBody('<strong>admin</strong>')
 
     def test_logout(self):
-        self.getPage('/login', method='POST', body='login=admin&password=admin')
+        self._login()
         self.getPage('/logout', headers=self.cookies)
         self.assertHeaderMatches('Set-Cookie', 'auth_tkt="INVALID"')
+
+    def test_opmuse_m3u(self):
+        auth_tkt = self._login()
+
+        self.getPage("/play/opmuse.m3u", headers=self.cookies)
+
+        self.assertHeader('Content-Type', 'audio/x-mpegurl')
+        self.assertMatchesBody('#EXTM3U')
+        self.assertMatchesBody('http://127.0.0.1:54583/play/stream\?auth_tkt=%s' % auth_tkt)
+
+    def test_stream(self):
+        auth_tkt = self._login()
+
+        self.getPage('http://127.0.0.1:54583/play/stream?auth_tkt=%s' % auth_tkt)
+
+        # queue is empty
+        self.assertStatus(409)
