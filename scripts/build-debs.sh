cf6548b488b9a5e18c4c9d4a2dde775f3743ff67
Mattias Fliesberg <mattias.fliesberg@gmail.com>
2014-05-25 12:59:54 +0200

    upgrade alembic to 0.6.5

--- scripts/build-debs.sh
+++ scripts/build-debs.sh
@@ -64,7 +64,7 @@ done
 # do that instead.
 build_git repoze.who 2.2 https://github.com/repoze/repoze.who.git
 build_git jinja2 2.7.2 https://github.com/mitsuhiko/jinja2.git
-build_git alembic rel_0_6_2 https://bitbucket.org/zzzeek/alembic.git
+build_git alembic rel_0_6_5 https://bitbucket.org/zzzeek/alembic.git
 build_git zope.interface 4.0.5 https://github.com/zopefoundation/zope.interface.git
 build_git watchdog v0.7.1 https://github.com/gorakhargosh/watchdog.git
 build_git sqlalchemy-utils 5b00373f https://github.com/kvesteri/sqlalchemy-utils.git # v0.26
