a7058b4c779797b6b447ba759fbdb8c1cf55819e
Mattias Fliesberg <mattias.fliesberg@gmail.com>
2014-08-08 12:41:01 +0200

    fix bug where albums with no track numbers would crash

--- opmuse/controllers/library.py
+++ opmuse/controllers/library.py
@@ -596,22 +596,29 @@ class Library:
                     for tag_name in remotes_artist['lastfm']['tags']:
                         remotes.update_tag(tag_name)
 
-        album_disc_nos = {}
+        disc_nos = {}
 
         for track in album.tracks:
             remotes.update_track(track)
 
-            if track.disc not in album_disc_nos:
-                album_disc_nos[track.disc] = []
+            if track.disc not in disc_nos:
+                disc_nos[track.disc] = []
 
             if track.number is not None:
                 # extract max track no, '01' will become 1, '01/10' will become 10
-                album_disc_nos[track.disc].append(int(re.search('\d+$', track.number).group()))
+                disc_nos[track.disc].append(int(re.search('\d+$', track.number).group()))
             else:
-                album_disc_nos[track.disc].append(track.number)
+                disc_nos[track.disc].append(track.number)
 
-        album_disc_nos = OrderedDict(sorted(album_disc_nos.items(),
-                                     key=lambda album_disc_no: album_disc_no[0]))
+        album_disc_nos = []
+
+        for disc, numbers in disc_nos.items():
+            max_number = max(numbers)
+
+            if max_number is not None:
+                album_disc_nos.append((disc, len(numbers), max_number))
+
+        album_disc_nos = sorted(album_disc_nos, key=lambda album_disc_no: album_disc_no[0])
 
         remotes_album = remotes.get_album(album)
 
