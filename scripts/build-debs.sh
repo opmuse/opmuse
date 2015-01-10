a83f21420b1e65f32f840e6346d30db977e07ddf
Mattias Fliesberg <mattias@fliesberg.email>
2015-01-10 14:06:30 +0100

    upgrade alembic to 0.7

--- scripts/build-debs.sh
+++ scripts/build-debs.sh
@@ -73,7 +73,7 @@ done
 # do that instead.
 build_git repoze.who 2.2 https://github.com/repoze/repoze.who.git
 build_git jinja2 2.7.3 https://github.com/mitsuhiko/jinja2.git
-build_git alembic rel_0_6_7 https://bitbucket.org/zzzeek/alembic.git
+build_git alembic rel_0_7_3 https://bitbucket.org/zzzeek/alembic.git
 build_git watchdog v0.7.1 https://github.com/gorakhargosh/watchdog.git
 build_git webob 1.4 https://github.com/Pylons/webob.git 1.4-2 # bump version to override broken package in jessie
 build_git cherrypy 3.6.0 https://github.com/cherrypy/cherrypy.git
