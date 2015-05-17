9e41d5c5d2a7ecb277957c78f37380f38d723a59
Mattias Fliesberg <mattias@fliesberg.email>
2015-05-17 20:48:27 +0200

    no need to build repoze.who and webob anymore

--- scripts/build-debs.sh
+++ scripts/build-debs.sh
@@ -72,11 +72,9 @@ done
 
 # these packages pypi builds are broken but building from git works, so let's
 # do that instead.
-build_git repoze.who 2.2 https://github.com/repoze/repoze.who.git
 build_git jinja2 2.7.3 https://github.com/mitsuhiko/jinja2.git
 build_git alembic rel_0_7_3 https://bitbucket.org/zzzeek/alembic.git
 build_git watchdog v0.7.1 https://github.com/gorakhargosh/watchdog.git
-build_git webob 1.4 https://github.com/Pylons/webob.git 1.4-2 # bump version to override broken package in jessie
 build_git cherrypy 3.7.0 https://github.com/cherrypy/cherrypy.git
 build_git deluge-client 1.0.2 https://github.com/JohnDoee/deluge-client.git
 
