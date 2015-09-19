92a60eca8e77abe3b57fd89d9762fc793ed10332
Mattias Fliesberg <mattias@fliesberg.email>
2015-09-19 12:35:59 +0200

    fpm did something so now all of our reqs build with pip
    e.g. we no longer need to build from the git repos of some projects

--- scripts/build-debs.sh
+++ scripts/build-debs.sh
@@ -9,26 +9,6 @@ fi
 
 repo=$1
 
-function build_git() {
-    rm -rf $1
-    git clone $3
-    cd $1
-    git checkout $2
-    cd ..
-
-    if [[ -r $1/setup.cfg ]]; then
-        # remove dev tags and such
-        sed -i 's/tag_build\s*=[^=]*/tag_build=/' $1/setup.cfg
-    fi
-
-    if [[ -n $4 ]]; then
-        sed -i "s/version\s*=[^=,]*/version='$4'/" $1/setup.py
-    fi
-
-    ./scripts/build-python-deb.sh $repo master $1/setup.py $1 none none none \
-        none none none none none none none none
-}
-
 reprepro -b $repo deleteunreferenced
 
 # build deb packages from requirements.txt files except the broken ones, they're
@@ -37,8 +17,7 @@ reprepro -b $repo deleteunreferenced
 #
 # note that we skip building mako, firepy, Sphinx and sphinx_rtd_theme
 # altogether. instead we use the os-provided ones
-grep -hiEv "repoze\.who|jinja2|alembic|mako|firepy|Sphinx|\
-    sphinx_rtd_theme|watchdog|WebOb|CherryPy|deluge-client|colorlog|nose|^#" \
+grep -hiEv "mako|firepy|Sphinx|sphinx_rtd_theme|colorlog|nose|^#" \
     requirements.txt mysql-requirements.txt dev-requirements.txt | \
 while read -A req; do
     if [[ -f $req[1] ]]; then
@@ -69,14 +48,6 @@ while read -A req; do
             none none none none none none none none none
     fi
 done
-
-# these packages pypi builds are broken but building from git works, so let's
-# do that instead.
-build_git jinja2 2.7.3 https://github.com/mitsuhiko/jinja2.git
-build_git alembic rel_0_7_7 https://bitbucket.org/zzzeek/alembic.git
-build_git watchdog v0.7.1 https://github.com/gorakhargosh/watchdog.git
-build_git cherrypy 3.8.0 https://github.com/cherrypy/cherrypy.git
-build_git deluge-client 1.0.2 https://github.com/JohnDoee/deluge-client.git
 
 # build opmuse deb package
 ./scripts/build-python-deb.sh $repo master setup.py opmuse none scripts/debian-before-install.sh \
